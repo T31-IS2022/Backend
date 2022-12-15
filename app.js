dbbb9be54f0d34334604ebfe7d47e4fb98920ded
Luca <luca.dematte2020@gmail.com>
2022-12-15 09:12:29 +0100

    Aggiunta configurazione server e mongodb

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+//includo dotenv per poter utilizzare comodamente un file con parametri impostabili
+require("dotenv").config();
+
+const express = require("express"); //includo express
+const app = express();
+
+console.log("App avviata");
+
+//metto express in ascolto sulla porta specificata nel file .env
+app.listen(process.env.PORT, () =>
+    console.log("App in ascolto sulla porta " + process.env.PORT)
+);
+
+//aggiungere routes
+
+//MONGODB
+//importo mongoose
+const mongoose = require("mongoose");
+
+//collegamento al database
+mongoose.connect(
+    process.env.DB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err)
+            return console.log("Errore nel collegamento al database: ", err);
+        console.log(
+            "Connessione a MongoDB -- Stato connessione:",
+            mongoose.connection.readyState
+        );
+    }
+);
