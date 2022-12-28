93e9c368160fd3fbc18287121eb143537d88365c
Luca <luca.dematte2020@gmail.com>
2022-12-28 16:20:41 +0100

    Aggiunto modulo Swagger per la documentazione delle API

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 
-// aggiunge le routes all'applicazione
-
-const express = require("express")
+const express = require("express");
 const app = express();
 
-console.log("started application");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-//aggiungere routes
+console.log("Application started");
+
+//routes
 const routesServizio = require("./routes/servizio");
 app.use("/servizio", routesServizio);
 
 const routesPrenotazione = require("./routes/prenotazione");
 app.use("/prenotazione", routesPrenotazione);
 
-const routesRicorrenza = require("./routes/ricorrenza")
+const routesRicorrenza = require("./routes/ricorrenza");
 app.use("/ricorrenza", routesRicorrenza);
 
 const routesSpazio = require("./routes/spazio");
@@ -23,18 +24,19 @@ app.use("/spazio", routesSpazio);
 const routesUtente = require("./routes/utente");
 app.use("/utente", routesUtente);
 
+//route per la documentazione fornita da Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 //metto express in ascolto sulla porta specificata nel file .env
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-    console.log("App in ascolto sulla porta " + PORT)
-);
+app.listen(PORT, () => console.log("App in ascolto sulla porta " + PORT));
 
 //MONGODB
 //importo mongoose
 const mongoose = require("mongoose");
 
 //collegamento al database
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 mongoose.connect(
     process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true },
