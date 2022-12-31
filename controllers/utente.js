f5041fca4c0687d95cbd6299268d6475e713f05d
Luca <luca.dematte2020@gmail.com>
2022-12-31 15:05:31 +0100

    Documentazione API utente completata, fix vari

--- controllers/utente.js
+++ controllers/utente.js
@@ -1,15 +1,12 @@
 //includo il modello dello spazio per poterlo usare qui
 const Utente = require("../models/utente");
-const jwt = require('jsonwebtoken');
-const {servizioMail: servizioMail} = require('../scripts/email');
+const jwt = require("jsonwebtoken");
+const { servizioMail: servizioMail } = require("../scripts/email");
 //includo l'ObjectId per poter cercare elementi tramite il loro ID
 var ObjectId = require("mongodb").ObjectId;
 
-function generaToken(data){
-    return jwt.sign(
-        {data}, 
-        process.env.JWT_KEY,
-        {expiresIn: 86400});
+function generaToken(data) {
+    return jwt.sign({ data }, process.env.JWT_KEY, { expiresIn: 86400 });
 }
 
 //FUNZIONI PER LE VARIE ROUTES
@@ -24,22 +21,26 @@ const listaUtenti = (req, res) => {
     let count = parseInt(req.query.count || 50);
     let start = parseInt(req.query.start || 0);
 
-    if (isNaN(count) || isNaN(start)){
-        return res.status(400).json({message: "start e count devono essere interi"});
+    if (isNaN(count) || isNaN(start)) {
+        return res
+            .status(400)
+            .json({ message: "start e count devono essere interi" });
     }
-    if (count<1 || start<0){
-        return res.status(400).json({message: "start deve essere positivo e count maggiore di 0"});
+    if (count < 1 || start < 0) {
+        return res.status(400).json({
+            message: "start deve essere positivo e count maggiore di 0",
+        });
     }
 
     //cerco gli spazi dando un filtro vuoto per ottenerli tutti
     Utente.find({}, (err, data) => {
         if (err) {
-            return res.status(500).json({code: 500, message: err }); //risposta in caso di errore
+            return res.status(500).json({ code: 500, message: err }); //risposta in caso di errore
         }
-        return res.status(data?200:204).json(data); //restituisco i dati di tutti gli spazi
+        return res.status(data ? 200 : 204).json(data); //restituisco i dati di tutti gli spazi
     })
-    .skip(start)
-    .limit(count);
+        .skip(start)
+        .limit(count);
 };
 
 //login dell'utente
@@ -52,7 +53,7 @@ const loginUtente = (req, res) => {
     );
 
     if (!req.body)
-        return res.status(400).json({code: 404, messsage: "Body mancante"});
+        return res.status(400).json({ code: 400, messsage: "Body mancante" });
 
     //prendo l'email dell'utente dai parametri della richiesta
     let email = req.body.email;
@@ -68,49 +69,57 @@ const loginUtente = (req, res) => {
 
     //cerco e restituisco l'utente con quella email e quella password
     Utente.findOne({ email: email, password: password }, (err, data) => {
-        if (err)
-            return res.status(500).json({code: 500, message: err});
+        if (err) return res.status(500).json({ code: 500, message: err });
         if (!data)
-            return res.status(404).json({code: 404, message: "Email o password errati" });
+            return res
+                .status(404)
+                .json({ code: 404, message: "Email o password errati" });
         if (!data.confermaAccount)
-            return res.status(400).json({code: 400, message: "L'utente non ha confermato l'account" });
-        
+            return res.status(400).json({
+                code: 400,
+                message: "L'utente non ha confermato l'account",
+            });
+
         token = generaToken(data);
-        return res.status(200).json({token: token}); 
+        return res.status(200).json({ token: token });
     });
 };
 
-const confermaUtente = (req,res)=>{
+const confermaUtente = (req, res) => {
     const id = req.query.id;
-    if (!id){
-        return res.status(400).json({code: 400, message: "Parametro mancante: id"});
+    if (!id) {
+        return res
+            .status(400)
+            .json({ code: 400, message: "Parametro mancante: id" });
     }
-    if (!ObjectId.isValid(id)){
-        return res.status(400).json({code: 400, message: "id invalido"});
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ code: 400, message: "id invalido" });
     }
 
-    Utente.findOne({_id: ObjectId(id)})
-    .then(data=>{
-        if (!data){
-            res.status(404).json({code: 404, message: `L'id '${id}' non corrisponde a nessun utente`});
-        }
-        data.confermaAccount=true;
-        data.save()
-        .then(data=>{
-            return res.status(200).json({code: 200, message: `L'utente con l'id '${id}' è stato confermato`});
+    Utente.findOne({ _id: ObjectId(id) })
+        .then((data) => {
+            if (!data) {
+                res.status(404).json({
+                    code: 404,
+                    message: `L'id '${id}' non corrisponde a nessun utente`,
+                });
+            }
+            data.confermaAccount = true;
+            data.save()
+                .then((data) => {
+                    return res.status(200).json({
+                        code: 200,
+                        message: `L'utente con l'id '${id}' è stato confermato`,
+                    });
+                })
+                .catch((err) => {
+                    return res.status(500).json({ code: 500, message: err });
+                });
         })
-        .catch(err=>{
-            return res.status(500).json({code: 500, message: err})
-        })
-    })
-    .catch(err=>{
-        return res.status(500).json({code:500, message: err})
-    });
-}
-
-const logoutUtente = (req,res)=>{
-    return res.status(200).json({message:"not implemented"});
-}
+        .catch((err) => {
+            return res.status(500).json({ code: 500, message: err });
+        });
+};
 
 //registrazione di un nuovo utente
 const registrazione = (req, res) => {
@@ -118,8 +127,10 @@ const registrazione = (req, res) => {
     console.log("Nuovo utente\n\tParametri: " + JSON.stringify(req.body));
 
     const body = req.body;
-    if (!body || !(body.email && body.nome && body.cognome && body.password)){
-        return res.status(400).json({code: 400, message: "Parametri mancanti"});
+    if (!body || !(body.email && body.nome && body.cognome && body.password)) {
+        return res
+            .status(400)
+            .json({ code: 400, message: "Parametri mancanti" });
     }
 
     const nome = body.nome;
@@ -132,8 +143,7 @@ const registrazione = (req, res) => {
 
     //controllo se un utente con questa email è già stato inserito nel database
     Utente.findOne({ email: email }, (err, data) => {
-        if (err) 
-            return res.status(500).json({code: 500, message:err});
+        if (err) return res.status(500).json({ code: 500, message: err });
         if (!data) {
             //se non l'ho inserito creo un nuovo utente con i dati provenienti dalla richiesta
             const nuovoUtente = new Utente({
@@ -150,29 +160,32 @@ const registrazione = (req, res) => {
 
             //salvo il nuovo utente nel database
             nuovoUtente.save((err, data) => {
-                if (err) 
-                    return res.status(500).json({code: 500, message: err }); //risposta in caso di errore
-                
+                if (err)
+                    return res.status(500).json({ code: 500, message: err }); //risposta in caso di errore
+
                 const id = data._id;
-                servizioMail.sendMail({
-                    from: `t31 <noreply.${process.env.EMAIL_ADDR}>`,
-                    replyTo: `noreply.${process.env.EMAIL_ADDR}`,
-                    to: email,
-                    subject: "Conferma account Makako",
-                    text: "Conferma il tuo account Makako per poter utilizzare il tuo account",
-                    html: `<h1>Conferma il tuo account Makako</h1><p>Attiva ora l'account ${nome} ${cognome}</p><br/><a href="${process.env.WEB_ADDR}/utente/conferma?id=${id}">Conferma!</a>`
-                }).then(info=>{
-                    console.log(info);
-                }).catch(err=>{
-                    console.log(err);
-                });
+                servizioMail
+                    .sendMail({
+                        from: `t31 <noreply.${process.env.EMAIL_ADDR}>`,
+                        replyTo: `noreply.${process.env.EMAIL_ADDR}`,
+                        to: email,
+                        subject: "Conferma account Makako",
+                        text: "Conferma il tuo account Makako per poter utilizzare il tuo account",
+                        html: `<h1>Conferma il tuo account Makako</h1><p>Attiva ora l'account ${nome} ${cognome}</p><br/><a href="${process.env.WEB_ADDR}/utente/conferma?id=${id}">Conferma!</a>`,
+                    })
+                    .then((info) => {
+                        console.log(info);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
                 return res.status(201).json(data); //risposta se l'utente è stato salvato nel database
             });
         } else {
             return res.status(409).json({
                 code: 409,
                 message: "E' già presente un utente con questa e-mail",
-            });            
+            });
         }
     });
 };
@@ -187,24 +200,28 @@ const getUtenteConEmail = (req, res) => {
     );
 
     //prendo l'email dell'utente dai parametri della richiesta
-    if (!req.query || !req.query.email){
-        return res.status(400).json({code: 400, message: "Email non specificata"});
+    if (!req.query || !req.query.email) {
+        return res
+            .status(400)
+            .json({ code: 400, message: "Email non specificata" });
     }
 
     let email = req.query.email;
-    if (req.utente.livello>=2 || req.utente.email==email){
+    if (req.utente.livello >= 2 || req.utente.email == email) {
         //cerco e restituisco l'utente con quella email
         Utente.findOne({ email: email }, (err, data) => {
-            if (err)
-                return res.status(500).json({code:500, message: err});
-            if (!data) 
-                return res
-                    .status(404)
-                    .json({code:400, message: "L'utente richiesto non esiste" });
+            if (err) return res.status(500).json({ code: 500, message: err });
+            if (!data)
+                return res.status(404).json({
+                    code: 400,
+                    message: "L'utente richiesto non esiste",
+                });
             return res.status(200).json(data); //se trovo l'oggetto lo restituisco
         });
-    }else{
-        return res.start(403).json({code: 403, message: "Untente non autorizzato"});
+    } else {
+        return res
+            .start(403)
+            .json({ code: 403, message: "Untente non autorizzato" });
     }
 };
 
@@ -218,21 +235,22 @@ const getUtenteConID = (req, res) => {
     );
 
     //prendo l'ID dell'utente dai parametri della richiesta
-    if (!req.query || !req.query.id){
-        return res.status(400).json({code: 400, message: "id non specificato"});
+    if (!req.query || !req.query.id) {
+        return res
+            .status(400)
+            .json({ code: 400, message: "id non specificato" });
     }
 
     let id = req.query.id;
     //cerco e restituisco l'utente con quell'ID'
 
-    if (!ObjectId.isValid(id)){
-        return res.status(400).json({code:400, smessage: "id invalido"});
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ code: 400, smessage: "id invalido" });
     }
 
     Utente.findOne({ _id: ObjectId(id) }, (err, data) => {
-        if (err)
-            return res.status(500).json({code:500, message:err});
-        if (!data) 
+        if (err) return res.status(500).json({ code: 500, message: err });
+        if (!data)
             return res.status(404).json({
                 code: 404,
                 message: "L'utente richiesto non esiste",
@@ -247,41 +265,46 @@ const modificaUtente = (req, res) => {
         "Modifica di un utente\n\tParametri: " + JSON.stringify(req.params)
     );
 
-    if (!req.params || !req.params.id){
-        return res.status(400).json({code: 400, message: "id non specificato"});
+    if (!req.params || !req.params.id) {
+        return res
+            .status(400)
+            .json({ code: 400, message: "id non specificato" });
     }
 
     //prendo l'ID dell'utente dai parametri della richiesta
     const id = req.params.id;
 
-    if (!ObjectId.isValid(id)){
-        return res.status(400).json({code: 400, message: "id invalido"});
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ code: 400, message: "id invalido" });
     }
 
     const body = req.body;
     if (!body || !(body.email && body.nome && body.cognome && body.password))
-        return res.status(400).json({code: 400, message: "Parametri mancanti"});
+        return res
+            .status(400)
+            .json({ code: 400, message: "Parametri mancanti" });
 
-    if (req.utente.livello<2 && req.utente._id != id)
-        return res.status(403).json({code: 403, message: "Utente non autorizzato"});
+    if (req.utente.livello < 2 && req.utente._id != id)
+        return res
+            .status(403)
+            .json({ code: 403, message: "Utente non autorizzato" });
 
     const nome = body.nome;
     const cognome = body.cognome;
     const email = body.email;
     const password = body.password;
-    const telefono = body.telefono||undefined;
-    const indirizzo = body.indirizzo||undefined;
-    const foto = body.URLfoto||undefined;
+    const telefono = body.telefono || undefined;
+    const indirizzo = body.indirizzo || undefined;
+    const foto = body.URLfoto || undefined;
 
     //cerco e modifico l'utente con quella email
     Utente.findOne({ _id: ObjectId(id) }, (err, data) => {
-        if (err)
-            return res.status(500).json({code:500, message: err});
-        if (!data) 
+        if (err) return res.status(500).json({ code: 500, message: err });
+        if (!data)
             return res
                 .status(404)
                 .json({ code: 400, message: "L'utente richiesto non esiste" });
-        
+
         //modifiche all'utente
         data.nome = nome;
         data.cognome = cognome;
@@ -293,10 +316,9 @@ const modificaUtente = (req, res) => {
 
         //salvo le modifiche
         data.save((err, data) => {
-            if (err) return res.status(500).json({code:500, Errore: err }); //risposta in caso di errore
+            if (err) return res.status(500).json({ code: 500, Errore: err }); //risposta in caso di errore
             return res.status(200).json(data); //risposta se l'utente è stato salvato nel database
         });
-    
     });
 };
 
@@ -307,34 +329,37 @@ const cancellaUtente = (req, res) => {
     );
 
     if (!req.params || !req.params.id)
-        return res.status(400).json({code:400, message: "id non specificato"});
+        return res
+            .status(400)
+            .json({ code: 400, message: "id non specificato" });
 
     //prendo l'ID dell'utente dai parametri della richiesta
     const id = req.params.id;
 
     if (!ObjectId.isValid(id))
-        return res.status(400).json({code:400, message: "id invalido"});
+        return res.status(400).json({ code: 400, message: "id invalido" });
 
-    if (req.utente.livello<2 && req.utente._id != id)
-        return res.status(403).json({code: 403, message: "Utente non autorizzato"});
+    if (req.utente.livello < 2 && req.utente._id != id)
+        return res
+            .status(403)
+            .json({ code: 403, message: "Utente non autorizzato" });
 
     //prima di eliminarlo verifico che l'utente esista
     Utente.findOne({ _id: ObjectId(id) }, (err, data) => {
-        if (err)
-            return res.status(500).json({code:500, message: err});
-        if (!data) 
-            return res.status(404).json({code:404, message: "L'utente richiesto non esiste" });
-        
+        if (err) return res.status(500).json({ code: 500, message: err });
+        if (!data)
+            return res
+                .status(404)
+                .json({ code: 404, message: "L'utente richiesto non esiste" });
+
         //cerco ed elimino l'utente con quell'ID
         Utente.deleteOne({ _id: ObjectId(id) }, (err) => {
-            if (err)
-                return res.status(500).json({code:500, message: err});
+            if (err) return res.status(500).json({ code: 500, message: err });
             return res.status(200).json({
-                code:200,
+                code: 200,
                 message: "Utente eliminato correttamente",
             });
         });
-    
     });
 };
 
@@ -347,6 +372,5 @@ module.exports = {
     listaUtenti,
     modificaUtente,
     cancellaUtente,
-    logoutUtente,
-    confermaUtente
+    confermaUtente,
 };
