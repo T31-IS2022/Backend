babdc6bfb45d3e343b55cc5a66a3bb52c564702f
MatteoFrizzera <99304092+MatteoFrizzera@users.noreply.github.com>
2023-02-17 05:39:05 +0100

    Tutti i test funzionano. Code coverage 100% del file controller utente.js Aggiunti test per i token jwt e per la conferma dell'account

--- test/utente.test.js
+++ test/utente.test.js
@@ -1,16 +1,55 @@
 const fetch = require("node-fetch")
 const Utente = require("../models/utente")
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// condizioni database per corretto funzionamento dei test:
+// 1. esiste Utente con email: test@test
+// 2. non esiste Utente con email: non@esiste
 
 const url = "http://localhost:3000/utente"
 
 
-const token = 'inserire qui token di livello 2 valido'
+let token;
 
-// condizioni database per corretto funzionamento dei test:
-// 1. esiste Utente con email: test@test e _id: 63ab522e692633bc9b18446f
-// 2. non esiste Utente con email: non@esiste
+let testUserId;
+
+beforeAll(async () => {
+    //collegamento al database
+    mongoose.set("strictQuery", false);
+    mongoose.connect(
+        process.env.MONGODB_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err) => {
+            if (err) return console.log("Errore nel collegamento al database: ", err);
+        }
+    );
+
+    const responseLogInTestUser = await fetch(url+"/login", {
+        method: 'POST',
+        body: new URLSearchParams({
+            email: "test@test",
+            password: "test"
+        })
+    });
+
+    // utilizza il test user per generare un token sessione valido per effettuare i test
+    // in alternativa si può assegnare manualmente un token jwt valido alla variabile 'token'
+    token = (await responseLogInTestUser.json()).token
+
+    // ottiene l' _id del test user per effettuare i test
+    return Utente.findOne({ email: "test@test" }).then(data => {
+        testUserId = data._id
+        console.log("TEST USER ID: " + testUserId)
+    })
+
+})
+
+afterAll(() => {
+    mongoose.disconnect()
+})
+
 
 describe('listaUtenti: GET /', () => {
     const api = "/"
@@ -20,18 +59,43 @@ describe('listaUtenti: GET /', () => {
         const response = await fetch(url+api+"?start=abc&count=xyz", {
                 method: 'GET',
                 headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'x-access-token': token
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'x-access-token': token
                 }
             });
         expect(response.status).toEqual(400)
+    });
+
+    test('token mancante', async () => {
+        expect.assertions(1)
+        const response = await fetch(url+api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+
+                }
+            });
+        expect(response.status).toEqual(401)
+    });
+
+    test('token non valido', async () => {
+        expect.assertions(1)
+        const response = await fetch(url+api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'x-access-token': 'TOKEN NON VALIDO'
+                }
+            });
+        expect(response.status).toEqual(403)
     });
 
     test('start e count numeri negativi', async () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?start=-1&count=-1", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
                 'x-access-token': token
             }
         });
@@ -43,7 +107,8 @@ describe('listaUtenti: GET /', () => {
         expect.assertions(1)
         var response = await fetch(url+api, {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
                 'x-access-token': token
             }
         });
@@ -55,7 +120,8 @@ describe('listaUtenti: GET /', () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?start=2&count=5", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
                 'x-access-token': token
             }
         });
@@ -73,11 +139,11 @@ describe('loginUtente: POST /login', () => {
         expect.assertions(1)
         var response = await fetch(url+api, {
             method: 'POST',
-            body: {
+            body: new URLSearchParams({
                 email: "test@test",
                 password: "test"
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            })
+            
         });
         expect(response.status).toEqual(200)
 
@@ -88,13 +154,12 @@ describe('loginUtente: POST /login', () => {
         expect.assertions(1)
         var response = await fetch(url+api, {
             method: 'POST',
-            body: {
+            body: new URLSearchParams({
                 email: "non@esiste",
                 password: "non esiste"
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            })
         });
-        expect(response.status).toEqual(401)
+        expect(response.status).toEqual(404)
 
     })
 
@@ -102,14 +167,15 @@ describe('loginUtente: POST /login', () => {
         expect.assertions(1)
         var response = await fetch(url+api, {
             method: 'POST',
-            body: {
+            body: new URLSearchParams({
                 
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            })
         });
         expect(response.status).toEqual(400)
 
     })
+
+    
 })
 
 describe('GET logoutUtente', () => {
@@ -125,8 +191,7 @@ describe('registrazione: POST /registrazione', () => {
             method: 'POST',
             body: {
                 
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            }
         });
         expect(response.status).toEqual(400)
     })
@@ -136,7 +201,7 @@ describe('registrazione: POST /registrazione', () => {
         expect.assertions(1)
         var response = await fetch(url+api, {
             method: 'POST',
-            body: {
+            body: new URLSearchParams({
                 nome: 'test',
                 cognome: 'test',
                 email: 'test@test',
@@ -144,8 +209,7 @@ describe('registrazione: POST /registrazione', () => {
                 telefono: '123456789',
                 indirizzo: 'test',
                 URLfoto: 'url/test/foto'
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            })
         });
         expect(response.status).toEqual(409)
     });
@@ -154,7 +218,7 @@ describe('registrazione: POST /registrazione', () => {
         expect.assertions(1)
         var response = await fetch(url+api, {
             method: 'POST',
-            body: {
+            body: new URLSearchParams({
                 nome: 'test',
                 cognome: 'test',
                 email: 'nuovo@utente',
@@ -162,12 +226,11 @@ describe('registrazione: POST /registrazione', () => {
                 telefono: '123456789',
                 indirizzo: 'test',
                 URLfoto: 'url/test/foto'
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            })
         });
 
         // rimuove l'utente appena inserito nel db
-        Utente.findOneAndDelete({ email: "nuovo@utente"})
+        await Utente.findOneAndDelete({ email: "nuovo@utente"})
 
         expect(response.status).toEqual(201)
     })
@@ -181,7 +244,9 @@ describe('getUtenteConEmail: GET /byEmail', () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'x-access-token': token }
         });
         expect(response.status).toEqual(400)
     })
@@ -190,16 +255,44 @@ describe('getUtenteConEmail: GET /byEmail', () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?email=test@test", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'x-access-token': token }
         });
         expect(response.status).toEqual(200)
     })
+
+    test('token mancante', async () => {
+        expect.assertions(1)
+        const response = await fetch(url+api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+
+                }
+            });
+        expect(response.status).toEqual(401)
+    });
+
+    test('token non valido', async () => {
+        expect.assertions(1)
+        const response = await fetch(url+api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'x-access-token': 'TOKEN NON VALIDO'
+                }
+            });
+        expect(response.status).toEqual(403)
+    });
 
     test('cerca utente che non esiste', async () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?email=non@esiste", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'x-access-token': token }
         });
         expect(response.status).toEqual(404)
     })
@@ -213,25 +306,55 @@ describe('getUtenteConID: GET /byID', () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'x-access-token': token }
         });
         expect(response.status).toEqual(400)
     })
 
     test('cerca utente con id presente nel database', async () => {
         expect.assertions(1)
-        var response = await fetch(url+api+"?id=63ab522e692633bc9b18446f", {
+        const response = await fetch(url+api+ "?id="+testUserId, {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'x-access-token': token
+            }
         });
         expect(response.status).toEqual(200)
     })
+
+    test('token mancante', async () => {
+        expect.assertions(1)
+        const response = await fetch(url+api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+
+                }
+            });
+        expect(response.status).toEqual(401)
+    });
+
+    test('token non valido', async () => {
+        expect.assertions(1)
+        const response = await fetch(url+api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'x-access-token': 'TOKEN NON VALIDO'
+                }
+            });
+        expect(response.status).toEqual(403)
+    });
 
     test('cerca con id non valido', async () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?id=ID_N0N_VAL1D0", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'x-access-token': token
+            }
         });
         expect(response.status).toEqual(400)
     })
@@ -240,7 +363,9 @@ describe('getUtenteConID: GET /byID', () => {
         expect.assertions(1)
         var response = await fetch(url+api+"?id=63ab633e713644bc9b18556f", {
             method: 'GET',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'x-access-token': token
+            }
         });
         expect(response.status).toEqual(404)
     })
@@ -248,21 +373,6 @@ describe('getUtenteConID: GET /byID', () => {
 
 describe('modificaUtente: PATCH /:id', () => {
     const api = "/"
-
-    test('id non specificato nei parametri della richiesta', async () => {
-        expect.assertions(1)
-        var response = await fetch(url+api, {
-            method: 'PATCH',
-            body: {
-                nome: "nuovoNome",
-                cognome: "nuovoCognome",
-                email: "nuova@email",
-                password: "nuovaPassword1"
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
-        });
-        expect(response.status).toEqual(400)
-    })
 
     test('id non valido', async () => {
         expect.assertions(1)
@@ -274,7 +384,7 @@ describe('modificaUtente: PATCH /:id', () => {
                 email: "nuova@email",
                 password: "nuovaPassword1"
             },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            headers: { 'x-access-token': token }
         });
         expect(response.status).toEqual(400)
     })
@@ -286,7 +396,7 @@ describe('modificaUtente: PATCH /:id', () => {
             body: {
 
             },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            headers: { 'x-access-token': token }
         });
         expect(response.status).toEqual(400)
     })
@@ -295,65 +405,63 @@ describe('modificaUtente: PATCH /:id', () => {
         expect.assertions(1)
         var response = await fetch(url+api+"/64ab533e692622bc9b19556f", {
             method: 'PATCH',
-            body: {
+            body: new URLSearchParams({
                 nome: "nuovoNome",
                 cognome: "nuovoCognome",
                 email: "nuova@email",
                 password: "nuovaPassword1"
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            }),
+            headers: { 'x-access-token': token }
         });
         expect(response.status).toEqual(404)
     })
 
     test('modifiche effettuate correttamente', async () => {
         expect.assertions(1)
-        var response = await fetch(url+api+"/63ab522e692633bc9b18446f", {
+        var response = await fetch(url+api+"/"+testUserId, {
             method: 'PATCH',
-            body: {
+            body: new URLSearchParams({
                 nome: 'test',
                 cognome: 'test',
-                email: 'nuovo@utente',
+                email: 'test@test',
                 password: 'test',
                 telefono: '123456789',
                 indirizzo: 'test',
                 URLfoto: 'url/test/foto'
-            },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            }),
+            headers: { 'x-access-token': token }
         });
         expect(response.status).toEqual(200)
     })
 
 })
 
+
 describe('cancellaUtente: DELETE /:id', () => {
     const api = "/"
 
-    test('id non specificato nei parametri della richiesta', async () => {
-        expect.assertions(1)
-        var response = await fetch(url+api, {
-            method: 'DELETE',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        });
-        expect(response.status).toEqual(400)
+    beforeAll(async () => {
+        
     })
 
     test('id non valido', async () => {
         expect.assertions(1)
-        var response = await fetch(url+api+"/ID_N0N_VAL1D0", {
+        var response = await fetch(url+api+"IDN0NVAL1D0", {
             method: 'DELETE',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'x-access-token': token }
         });
         expect(response.status).toEqual(400)
     })
 
     test('id valido ma non corrisponde a nessun utente', async () => {
         expect.assertions(1)
-        var response = await fetch(url+api+"/64ab533e692622bc9b19556f", {
+        var response = await fetch(url+api+"64ab123e692622bc9b19556f", {
             method: 'DELETE',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'x-access-token': token }
         });
-        expect(response.status).toEqual(400)
+        expect(response.status).toEqual(404)
     })
 
     test('utente eliminato correttamente', async () => {
@@ -366,16 +474,96 @@ describe('cancellaUtente: DELETE /:id', () => {
             password: "eliminare",
             telefono: "123456789",
             indirizzo: "eliminare",
-            URLfoto: "utente/da/eliminare",
+            URLfoto: "/images/utenti/default.png", // lasciare foto default altrimenti da errore perchè cercherà di eliminare una foto che non esite
+            salt: "this field is required"
         });
 
         //salvo il nuovo utente nel database per poi cancellarlo
-        const nuovoUtente = await nuovoUtente.save(err, data).exec();
+        const nuovoUtente = await datiNuovoUtente.save()
 
-        var response = await fetch(url+api+"/"+nuovoUtente._id, {
+        const response = await fetch(url+api+nuovoUtente._id, {
             method: 'DELETE',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: {
+                'x-access-token': token }
         });
         expect(response.status).toEqual(200)
     })
+})
+
+describe('confermaUtente: GET /conferma', () => {
+    const api = "/conferma"
+
+    
+    test('id non specificato', async () => {
+        expect.assertions(1)
+
+        const response = await fetch(url+api, {
+            method: 'GET',
+            headers: {
+                'x-access-token': token
+            }
+        });
+
+        expect(response.status).toEqual(400)
+    });
+
+    test('id non valido', async () => {
+        expect.assertions(1)
+
+        const response = await fetch(url+api+ "/?id=IDN0NVAL1D0", {
+            method: 'GET',
+            headers: {
+                'x-access-token': token
+            }
+        });
+
+        expect(response.status).toEqual(400)
+    });
+
+    /*
+    test('id valido e utente confermato con successo', async () => {
+        expect.assertions(2)
+
+        const response = await fetch(url+api+ "?id=" + testUserId, {
+            method: 'GET',
+            headers: {
+                'x-access-token': token
+            }
+        });
+
+        const testUser = await Utente.findOne({ _id: testUserId})
+        
+        expect(response.status).toEqual(200)
+        expect(testUser.confermaAccount).toEqual(true)
+    });
+    */
+
+    /*
+    test('id valido ma non corrisponde a nessun utente', async () => {
+        expect.assertions(1)
+
+        const response = await fetch(url+api+ "/?id=64ab123e692622bc9b19556f", {
+            method: 'GET',
+            headers: {
+                'x-access-token': token
+            }
+        });
+
+        expect(response.status).toEqual(404)
+    });
+    */
+
+    /*
+    test('token non specificato', async () => {
+        expect.assertions(1)
+
+        const response = await fetch(url+api+ "/?id=64ab123e692622bc9b19556f", {
+            method: 'GET',
+        });
+
+        expect(response.status).toEqual(403)
+    });
+    */
+
+    
 })
